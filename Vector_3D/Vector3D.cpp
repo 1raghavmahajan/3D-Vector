cbd9e9c19645363a8e2329a7ea33218df3878dd2
Roger <raghav.intrigue@gmail.com>
2018-11-29 11:25:54 +0530

    Added Member Functions

--- Vector_3D/Vector3D.cpp
+++ Vector_3D/Vector3D.cpp
@@ -1,14 +1,46 @@
 #include "Vector3D.h"
 
+
 //Default initialisations
 Vector3D::Vector3D(): x(0), y(0), z(0)
 {
 }
 
+Vector3D::Vector3D(double x, double y, double z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+Vector3D::Vector3D(const Vector3D& v2)
+{
+	x = v2.x;
+	y = v2.y;
+	z = v2.z;
+}
+
+
+
+std::ostream& operator<<(std::ostream& out, const Vector3D& v)
+{
+	out << "< " << v.x << " , " << v.y << " , " << v.z << " >";
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, Vector3D v)
+{
+	in >> v.x >> v.y >> v.z;
+	return in;
+}
+
+
+
 bool Vector3D::operator==(const Vector3D& v2) const
 {
 	return (x==v2.x && y==v2.y && z==v2.z);
 }
+
 
 Vector3D Vector3D::operator+(const Vector3D& v2) const
 {
@@ -27,6 +59,7 @@ Vector3D& Vector3D::operator+=(const Vector3D& v2)
 	return *this;
 }
 
+
 Vector3D Vector3D::operator-(const Vector3D& v2) const
 {
 	Vector3D t;
@@ -43,6 +76,7 @@ Vector3D& Vector3D::operator-=(const Vector3D& v2)
 	z-=v2.z;
 	return *this;
 }
+
 
 Vector3D Vector3D::operator*(double scalar) const
 {
@@ -61,7 +95,8 @@ Vector3D& Vector3D::operator*=(double scalar)
 	return *this;
 }
 
-Vector3D Vector3D::operator*(const Vector3D& v2)
+
+Vector3D Vector3D::operator*(const Vector3D& v2) const
 {
 	Vector3D t;
 	t.x = x * v2.x;
@@ -75,14 +110,57 @@ Vector3D& Vector3D::operator*=(const Vector3D& v2)
 	x*=v2.x;
 	y*=v2.y;
 	z*=v2.z;
+	return *this;
 }
 
-std::ostream& operator<<(std::ostream& out, const Vector3D& v)
+
+Vector3D Vector3D::operator/(double scalar) const
 {
-	out << "< " << v.x << " , " << v.y << " , " << v.z << " >";
+	Vector3D t;
+	t.x = x * scalar;
+	t.y = y * scalar;
+	t.z = z * scalar;
+	return t;
 }
 
-std::istream& operator>>(std::istream& in, Vector3D v)
+Vector3D& Vector3D::operator/=(double scalar)
 {
-	in >> v.x >> v.y >> v.z;
+	x/=scalar;
+	y/=scalar;
+	z/=scalar;
+	return *this;
 }
+
+
+
+Vector3D Vector3D::cross_product(const Vector3D &v2) const
+{
+    double ni = y*v2.z - z*v2.y;
+    double nj = z*v2.x - x*v2.z;
+    double nk = x*v2.y - y*v2.x;
+    return Vector3D (ni, nj, nk);
+}
+
+double Vector3D::mag() const
+{
+    return sqrt(mag_square());
+}
+
+double Vector3D::mag_square() const
+{
+    return x*x+y*y+z*z;
+}
+
+Vector3D Vector3D:: normalize()
+{
+    assert(mag()!=0);
+    *this/=mag();
+    return *this;
+}
+
+double Vector3D::distance(const Vector3D &v2) const
+{
+    Vector3D dist=*this-v2;
+    return dist.mag();
+}
+
--- Vector_3D/Vector3D.h
+++ Vector_3D/Vector3D.h
@@ -2,35 +2,24 @@
 
 #include <iostream>
 #include <vector>
+#include <cassert>
 //#include <cmath>
 
 class Vector3D
 {
-
-	double x, y, z;
-
 public:
 
-	// Constructor
+	/* 
+	 * I didn't see any reason to make the vector components private. 
+	 * Any client could in any case independently modify them via a "Set" method. 
+	 * As, there's no internal state to maintain or no invariant to protect, it's better to make them private for better code readability.
+	 */
+	double x, y, z;
+
+	// Constructors
 	Vector3D();
-
-	//Operator Overloading
-	bool operator == (const Vector3D& v2) const;
-	
-	Vector3D operator + (const Vector3D& v2) const;
-	Vector3D& operator += (const Vector3D& v);
-	
-	Vector3D operator - (const Vector3D& v2) const;
-	Vector3D& operator -= (const Vector3D& v);
-
-	// Scalar Multiplication
-	Vector3D operator * (double scalar) const;
-	Vector3D& operator *= (double scalar);
-	
-	// Dot product
-	Vector3D operator * (const Vector3D& v2);
-	Vector3D& operator *= (const Vector3D& v);
-
+	Vector3D(double x, double y, double z);
+	Vector3D(const Vector3D &v2);
 
 	//IO
 
@@ -38,6 +27,48 @@ public:
 	friend std::ostream &operator << (std::ostream &out, const Vector3D &v);
 	//Input Vector3D
 	friend std::istream &operator >> (std::istream &out, Vector3D v);
+
+
+	//Operator Overloading
+
+	bool operator == (const Vector3D& v2) const;
+	
+	Vector3D operator + (const Vector3D& v2) const;
+	Vector3D& operator += (const Vector3D& v);
+	
+	Vector3D operator - (const Vector3D& v2) const;
+	Vector3D& operator -= (const Vector3D& v2);
+
+	// Scalar Multiplication
+	Vector3D operator * (double scalar) const;
+	Vector3D& operator *= (double scalar);
+
+	// Dot Product
+	Vector3D operator * (const Vector3D& v2) const;
+	Vector3D& operator *= (const Vector3D& v2);
+
+	// Scalar Division
+	Vector3D operator / (double scalar) const;
+	Vector3D& operator /= (double scalar);
+
+
+	//Member Functions
+
+	//Magnitude Square |v|^2
+	double mag_square() const;
+
+	// Magitude / Absolute |v|
+	double mag() const;
+
+	// Normalize v = v / |v|
+	Vector3D normalize();
+
+	// Cross Product
+	Vector3D cross_product(const Vector3D& v2) const;
+
+	// 3D Distance |v - v2|
+	double distance(const Vector3D& v2) const;
+
 
 };
 
